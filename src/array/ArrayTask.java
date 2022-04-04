d1543f5206e46f042e6f07b4ee0b5f648dc56d94
m0stova <alesia.mostovaya@gmail.com>
2022-04-04 23:25:57 +0200

    array-task added

---
+++ src/array/ArrayTask.java
@@ -0,0 +1,45 @@
+package array;
+
+public class ArrayTask {
+
+    public static void main(String[] args) {
+
+        String word1[] = new String[]{"ab", "c"};
+        String word2[] = new String[]{"a", "bc"};
+
+        String word3[] = new String[]{"abgf", "c"};
+        String word4[] = new String[]{"afg", "bc"};
+
+        String word5[] = new String[]{"abc", "d", "defg"};
+        String word6[] = new String[]{"abcddefg"};
+
+
+        ArrayTask arrayComparison1 = new ArrayTask();
+        arrayComparison1.arrayStringsAreEqual(word1, word2);
+
+        ArrayTask arrayComparison2 = new ArrayTask();
+        arrayComparison2.arrayStringsAreEqual(word3, word4);
+
+        ArrayTask arrayComparison3 = new ArrayTask();
+        arrayComparison3.arrayStringsAreEqual(word5, word6);
+
+    }
+
+    public boolean arrayStringsAreEqual (String[] word1, String[] word2) {
+        String str1 = "";
+        String str2 = "";
+
+        for (int i = 0; i < word1.length; i++)
+            str1 += word1[i];
+
+        for (int i = 0; i < word2.length; i++)
+            str2 += word2[i];
+
+        System.out.println();
+        System.out.println("Are these 2 arrays equals? - " + str1.equals(str2));
+
+        return str1.equals(str2);
+    }
+
+
+}
